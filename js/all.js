5ae605b1a15221f841c94d4011b2beafe41f6ce3
huai-sian <bpud50122@gmail.com>
2025-05-31 16:24:03 +0800

    更新首頁結構與樣式，調整載入動畫時間，優化滾動效果，並新增技能與作品展示部分。

--- js/all.js
+++ js/all.js
@@ -1,65 +1,71 @@
-$(document).ready(function() {
-    const loading = document.querySelector('.loading');
-    (function (){
-        setTimeout(() => {
-            loading.style.display = 'none';
-        }, 2500)
-    }())
-    function debounce(func, wait=20, immediate = true){
-        var timeout;
-        return function(){
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        }
-    }
-    const ani_item = document.querySelectorAll('.animate_fade');
-    $('#demo01').animatedModal();
-    $('#demo02').animatedModal({modalTarget:'Modal02'});
-    $('#demo03').animatedModal({modalTarget:'Modal03'});
-    $('#demo04').animatedModal({modalTarget:'Modal04'}); 
-    $('#demo05').animatedModal({modalTarget:'Modal05'}); 
-    $('#demo06').animatedModal({modalTarget:'Modal06'}); 
-    $('#demo08').animatedModal({modalTarget:'Modal08'}); 
-    $('#demo09').animatedModal({modalTarget:'Modal09'}); 
-    $('#demo10').animatedModal({modalTarget:'Modal10'});
-    var topMenuHeight = $('header').height();
-    $('.nav-link').click(function(e){
-        let href=$(this).attr('href');
-        offsetTop=href==='#' ? 0 : $(href).offset().top-topMenuHeight;
-        $('html, body').stop().animate({
-            scrollTop: offsetTop
-        }, 800);
-        e.preventDefault();
-    });
-    $('.scrollico').click(function(e){
-        let href=$(this).attr('href');
-        offsetTop=href==='#' ? 0 : $(href).offset().top-topMenuHeight;
-        $('html, body').stop().animate({
-            scrollTop: offsetTop
-        }, 800);
-        e.preventDefault();
-    })
-    /* $('.about_con').smoove({moveY:'60%'});
+$(document).ready(function () {
+  const loading = document.querySelector(".loading");
+  (function () {
+    setTimeout(() => {
+      loading.style.display = "none";
+    }, 1000);
+  })();
+  function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+      var context = this,
+        args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+  const ani_item = document.querySelectorAll(".animate_fade");
+  $("#demo01").animatedModal();
+  $("#demo02").animatedModal({ modalTarget: "Modal02" });
+  $("#demo03").animatedModal({ modalTarget: "Modal03" });
+  $("#demo04").animatedModal({ modalTarget: "Modal04" });
+  $("#demo05").animatedModal({ modalTarget: "Modal05" });
+  $("#demo06").animatedModal({ modalTarget: "Modal06" });
+  $("#demo08").animatedModal({ modalTarget: "Modal08" });
+  $("#demo09").animatedModal({ modalTarget: "Modal09" });
+  $("#demo10").animatedModal({ modalTarget: "Modal10" });
+  var topMenuHeight = $("header").height();
+  $(".nav-link").click(function (e) {
+    let href = $(this).attr("href");
+    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight;
+    $("html, body").stop().animate(
+      {
+        scrollTop: offsetTop,
+      },
+      800
+    );
+    e.preventDefault();
+  });
+  $(".scrollico").click(function (e) {
+    let href = $(this).attr("href");
+    offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight;
+    $("html, body").stop().animate(
+      {
+        scrollTop: offsetTop,
+      },
+      800
+    );
+    e.preventDefault();
+  });
+  /* $('.about_con').smoove({moveY:'60%'});
     $('.skillcon').smoove({moveY:'60%'});
     $('.contact_con').smoove({moveY:'40%'}); */
-    function checkSlide(){
-        ani_item.forEach(item=>{
-            const scrollPos = window.innerHeight + window.scrollY;
-            if (scrollPos >= item.offsetTop) {
-                item.classList.add('fade_up');
-            }
-        })
-    };
-    window.addEventListener('scroll', debounce(checkSlide));
-    // document.querySelector('.scrolldown a').addEventListener('click', function(e){
-    //     e.preventDefault();
-    // }, false)
-
+  function checkSlide() {
+    ani_item.forEach((item) => {
+      const scrollPos = window.innerHeight + window.scrollY;
+      if (scrollPos >= item.offsetTop) {
+        item.classList.add("fade_up");
+      }
+    });
+  }
+  window.addEventListener("scroll", debounce(checkSlide));
+  // document.querySelector('.scrolldown a').addEventListener('click', function(e){
+  //     e.preventDefault();
+  // }, false)
 });
